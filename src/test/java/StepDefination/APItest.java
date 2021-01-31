9b73f21df5686932522050d26fb885a0ba7a90fc
Prakash <prakashmkabadi@gmail.com>
2021-01-31 14:56:45 -0600

    API Gradle project

---
+++ src/test/java/StepDefination/APItest.java
@@ -0,0 +1,37 @@
+package StepDefination;
+
+
+import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+
+
+public class APItest {
+	
+	@Given("Get request call works")
+	public void get_request_call_works() {
+	    System.out.println("Step Defination working");
+	    //throw new io.cucumber.java.PendingException();
+	    RestAssured.baseURI="https://reqres.in/api/users";
+	  //Request object
+	  RequestSpecification httprequest=RestAssured.given();
+
+	  //Response Object
+	  Response response= httprequest.request(Method.GET);
+
+	  //Print  Response in Console
+	  String responseBody= response.getBody().asString();
+	  System.out.println("Response Body is" +responseBody);
+
+
+	
+	    
+	    
+	    
+	}
+	
+
+}
---
+++ src/test/resources/Features/FirstAPI.feature
@@ -0,0 +1,12 @@
+Feature: API Sceanrios1
+
+@API
+Scenario Outline: API Test Scenarios  
+
+	Given Get request call works 
+	
+	#When Post request call works 
+	
+	Examples: 
+		| username | password |
+		|user1     |password1|
